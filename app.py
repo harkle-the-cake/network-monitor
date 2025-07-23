15b2f3e9a5b5bb1fbdcb74f9ff9048b41a07c8bc
Pat <harkle@gmx.net>
2025-07-23 10:58:08 +0200

    showing index page

--- app.py
+++ app.py
@@ -62,5 +62,10 @@ def hosts():
         all_hosts.extend(hosts)
     return jsonify(all_hosts)
 
+# Route: Indexseite
+@app.route("/")
+def index():
+    return send_from_directory(app.static_folder, "index.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
